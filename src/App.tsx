7b39f339b47ca1e622e2bcb5cdbbc2a0a77c57d8
soy eddy <soyeddy.email.com>
2024-12-15 22:57:27 -0600

    creditos

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,41 @@
-
-import { Background } from './components/Background';
-import { Header } from './components/Header';
-import { PartyDetails } from './components/PartyDetails';
-import { Countdown } from './components/Countdown';
-import { FadeIn } from './components/animations/FadeIn';
-import { BalloonsAndConfetti } from './components/animations/Ballons';
+import { Background } from "./components/Background";
+import { Header } from "./components/Header";
+import { PartyDetails } from "./components/PartyDetails";
+import { Countdown } from "./components/Countdown";
+import { FadeIn } from "./components/animations/FadeIn";
+import { BalloonsAndConfetti } from "./components/animations/Ballons";
 
 function App() {
   return (
-    <Background>
-      <BalloonsAndConfetti />
-      <div className="max-w-6xl mx-auto">
-        <Header />
-        
-        <FadeIn delay={1000}>
-          <Countdown />
-        </FadeIn>
-        
-        <FadeIn delay={1200}>
-          <PartyDetails />
-        </FadeIn>
-      
+    <>
+      {" "}
+      <Background>
+        <BalloonsAndConfetti />
+        <div className="max-w-6xl mx-auto">
+          <Header />
 
-        <FadeIn delay={1600}>
-          <footer className="text-center mt-20 text-gray-400">
-            <p>Con amor,</p>
-            <p className="text-2xl font-script mt-2 text-amber-300">Betsy Aguilar</p>
-          </footer>
-        </FadeIn>
-      </div>
-    </Background>
+          <FadeIn delay={1000}>
+            <Countdown />
+          </FadeIn>
+
+          <FadeIn delay={1200}>
+            <PartyDetails />
+          </FadeIn>
+
+          <FadeIn delay={1600}>
+            <footer className="text-center mt-20 text-gray-400">
+              <p>Con amor,</p>
+              <p className="text-2xl font-script mt-2 text-amber-300">
+                Betsy Aguilar
+              </p>
+            </footer>
+          </FadeIn>
+        </div>
+      </Background>
+      <footer className="text-sm font-script mt-2 text-amber-300 pb-2 text-center">
+        Create By Eddy Talavera
+      </footer>
+    </>
   );
 }
 
